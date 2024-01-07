365105b5d9a456dd0d7f44f414af25dd721d57c4
Leonid Markovski <leonid.markovski96@gmail.com>
2024-01-07 16:17:31 +0100

    The input is empty after the user searches for a city

--- Weather_APP/src/main.ts
+++ Weather_APP/src/main.ts
@@ -3,12 +3,11 @@ import { renderWeather } from "./functions";
 
 const submitFormEl=document.querySelector<HTMLFormElement>("#submitForm")
 const cityEl=document.querySelector<HTMLInputElement>("#city")
-submitFormEl!.addEventListener('submit',async (e)=>{
-const target=e.target as HTMLButtonElement
-const cityInput:string=cityEl!.value
+submitFormEl!.addEventListener('submit',async ()=>{
+let cityInput:string=cityEl!.value
 const dataWeather= await getApiData(cityInput)
 renderWeather(dataWeather)
-
+cityEl!.value=""
 })
 
 
